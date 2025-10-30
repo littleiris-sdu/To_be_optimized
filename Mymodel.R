6bb8fb80ecac88425d69cdb6e3893cf4a43357aa
littleiris-sdu <2069392093@qq.com>
2025-10-30 19:29:45 +0800

    Update Mymodel.R

--- Mymodel.R
+++ Mymodel.R
@@ -4,8 +4,8 @@ library(RcppArmadillo)
 Rcpp::sourceCpp("./Mymodel/Specific.cpp",verbose = TRUE, rebuild = TRUE)
 #Rcpp::sourceCpp("./Mymodel/Share.cpp",verbose = TRUE, rebuild = TRUE)
 
-  # 列表/数据框转数值向量 y1,y2 
-  # 基因矩阵转换 m1,m2
+  # Phenotype Vector y1,y2
+  # Gene matrix m1,m2
   g=ncol(m1)
   n1=length(y1)
   n2=length(y2)
@@ -15,11 +15,12 @@ Rcpp::sourceCpp("./Mymodel/Specific.cpp",verbose = TRUE, rebuild = TRUE)
   loglik0 <- H1$nullloglik_max
   MA1lkvec <- H1$loglkmaxs
   LRT=c()
-  
+
+  # Mixture chisq test
   for(k in 1:g){
     LRT[k]=2*(loglik0 - MA1lkvec[k])
     P[k]=0.5*pchisq(LRT[k], 1, lower.tail=FALSE)+0.25*pchisq(LRT[k], 2, lower.tail=FALSE)
   }
   
 listH=list(,loglik0,MA1lkvec,P)
-#each gene's ancestry-specific p value is in P
+#each gene's ancestry-specific/shared p value is in P
