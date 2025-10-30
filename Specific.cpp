f562bf939292adfd3a0374b64fba1e238292fe51
littleiris-sdu <2069392093@qq.com>
2025-10-30 19:45:50 +0800

    Update Specific.cpp

--- Specific.cpp
+++ Specific.cpp
@@ -1,9 +1,6 @@
 
 #define ARMA_64BIT_WORD 
 #include <RcppArmadillo.h>
- 
-#include <RcppArmadillo.h>
-// [[Rcpp::depends(RcppArmadillo)]]
 
 #include <R.h>
 #include <Rmath.h>
@@ -24,8 +21,7 @@ using namespace std;
 
 #include <armadillo>
 
-//ÒÑÖªM1ºÍM2£¬¹¹Ôìdiag(M1,M2)µÄ·Ö¿é¶Ô½Ç¾ØÕóº¯Êı£º 
-// ·Ö¿é¶Ô½Ç¾ØÕó¹¹Ôìº¯Êı (ÓÅ»¯ÄÚ´æ·ÖÅä)
+
 arma::mat construct_block_diag(const arma::mat& M1, const arma::mat& M2) {
     int n1 = M1.n_rows, n2 = M2.n_rows;
     int p1 = M1.n_cols, p2 = M2.n_cols;
@@ -36,13 +32,9 @@ arma::mat construct_block_diag(const arma::mat& M1, const arma::mat& M2) {
 }
  
 arma::mat construct_block(const arma::mat& A, const arma::mat& B, const arma::mat& C, const arma::mat& D){
-    // ´´½¨Áã¾ØÕó¿é 
-    // 2. Ë®Æ½Æ´½Ó£ººÏ²¢ĞĞ·½ÏòÏàÁÚµÄ¾ØÕó 
     arma::mat Upper = arma::join_horiz(A, B);  // [A | B]
     arma::mat Lower = arma::join_horiz(C, D);  // [C | D]
-    // 3. ´¹Ö±Æ´½Ó£ººÏ²¢ÁĞ·½ÏòµÄ·Ö¿é 
     arma::mat M = arma::join_vert(Upper, Lower);  // [A B; C D]
-    // ×İÏòÆ´½Ó£º×éºÏÉÏÏÂ²¿·Ö 
     return M;
 }
 
@@ -57,43 +49,40 @@ List Mymodel_PXEM_NULL_individualcpp(
 	const int n2,
 	const double tol,
     const int maxIter){     
- // const std::string mode,        // Ä£Ê½£¨null/alternative£©
- // const bool verbose             // µ÷ÊÔÊä³ö 
 	int g = M1.n_cols;
 	int N = n1+n2;
-	//²ÎÊı³õÊ¼»¯ 
+	//å‚æ•°åˆå§‹åŒ– 
 	arma::mat D1 = 0.01/g * arma::eye<arma::mat>(g, g);
 	arma::mat D2 = 0.01/g * arma::eye<arma::mat>(g, g);
 	arma::mat R = arma::zeros<arma::mat>(g, g);
-	double lambda = 1.0;        // lambdaÀ©Õ¹²ÎÊı³õÊ¼Öµ-¹Ì¶¨
-	double lambda2 = 1.0;        // lambdaÀ©Õ¹²ÎÊı³õÊ¼Öµ-¹Ì¶¨
-	double sigma2y1 = 1.0; // sd1·½²î
-	double sigma2y2 = 1.0; // sd2·½²î³õ
+	double lambda = 1.0;       
+	double lambda2 = 1.0;       
+	double sigma2y1 = 1.0; // sd1
+	double sigma2y2 = 1.0; // sd2
 	double sigma2y1_inv = 1.0; 
 	double sigma2y2_inv = 1.0; 
-	
-	    //¶¨ÒåÑ­»·ÖĞÊ¹ÓÃµÄÖĞ¼ä±äÁ¿ 
+
     arma::mat M2TM2 = M2.t() * M2; 
     arma::mat M2TY2 = M2.t() * Y2; 
 
 	arma::mat Sigmaa_inv,SigmaA;
 	double Tr1, Tr2, l1, l2, l3, l4, fenmu2, fenmu1, fenzi;
-	arma::vec zeros_col = arma::zeros<arma::vec>(g); // È« 0 ÁĞ
+	arma::vec zeros_col = arma::zeros<arma::vec>(g); //  0
 
  
-	arma::mat Sigmaa = construct_block(D1, R, R.t(), D2); //Æ´½Ó ¦²a 
+	arma::mat Sigmaa = construct_block(D1, R, R.t(), D2); //Î£a 
 
 //	cout << Sigmay_inv.n_cols <<Sigmay_inv.n_rows << endl;
 //	cout << Sigmaa.n_cols << Sigmaa.n_rows<< endl;
-	
-	//ËÆÈ»Ïà¹Ø±äÁ¿ 
+
+
 	int iter = 0;	
 	arma::vec loglik(maxIter, fill::zeros);
     double logdet1 = 0;
-	double sign1 = 0;                       // ĞĞÁĞÊ½¼ÆËã¸¨Öú±äÁ¿ 
+	double sign1 = 0;                       
 	double logdet2 = 0; 
 	double sign2 = 0;
-    int final_iter; // ¼ÇÂ¼Êµ¼Êµü´ú´ÎÊı
+    int final_iter;
 	
 	arma::mat M1TM1 = M1.t() * M1; 
     arma::mat M1TY1 = M1.t() * Y1; 
@@ -110,27 +99,23 @@ List Mymodel_PXEM_NULL_individualcpp(
 	arma::vec muA1,muA2;
 	
 	
-	//Ö÷Ñ­»·
+	//Main loop
 	for (int iter = 1; iter < maxIter; iter ++ ){
       // E-step 
-      // 1. ¸üĞÂºóÑéĞ­·½²î¾ØÕóSigmaA(·Ö¿éĞ­·½²îÕó) 
-      //------------------
-      // E-step 
-      // 1. ¸üĞÂºóÑéĞ­·½²î¾ØÕóSigmaA(·Ö¿éĞ­·½²îÕó) 
+     
         if(Sigmaa.is_sympd()){
-        Sigmaa_inv = arma::inv_sympd(Sigmaa);  // ¶Ô³ÆÕı¶¨¾ØÕó×¨ÓÃ 
+        Sigmaa_inv = arma::inv_sympd(Sigmaa);  
          } else {
         cout << "***** Sigmaa PINV *********************************************" << endl;
-        Sigmaa_inv =arma::inv(Sigmaa, arma::inv_opts::allow_approx);  // Î±Äæ/½üËÆÄæ¾ØÕó::inv(SigmaA_inv, arma::inv_opts::allow_approx)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        Sigmaa_inv =arma::inv(Sigmaa, arma::inv_opts::allow_approx);  // ::inv(SigmaA_inv, arma::inv_opts::allow_approx)!!!!!!!!!
         } 
         
         
-      arma::mat Sigmaa_inv_1 = Sigmaa_inv.submat(0,  0, g-1, g-1);    // ×óÉÏ¿é
-      arma::mat Sigmaa_inv_2 = Sigmaa_inv.submat(0,  g, g-1, 2*g-1);  // ÓÒÉÏ¿é
-      arma::mat Sigmaa_inv_3 = Sigmaa_inv_2.t();                      // ×óÏÂ¿é
-      arma::mat Sigmaa_inv_4 = Sigmaa_inv.submat(g,  g, 2*g-1, 2*g-1);// ÓÒÏÂ¿é 
-      
-            
+      arma::mat Sigmaa_inv_1 = Sigmaa_inv.submat(0,  0, g-1, g-1);    
+      arma::mat Sigmaa_inv_2 = Sigmaa_inv.submat(0,  g, g-1, 2*g-1);  
+      arma::mat Sigmaa_inv_3 = Sigmaa_inv_2.t();                      
+      arma::mat Sigmaa_inv_4 = Sigmaa_inv.submat(g,  g, 2*g-1, 2*g-1);
+          
       arma::mat SigmaA_inv_1 = sigma2y1_inv * M1TM1 + Sigmaa_inv_1;
       arma::mat SigmaA_inv_2 = Sigmaa_inv_2;
       arma::mat SigmaA_inv_3 = Sigmaa_inv_3;
@@ -139,22 +124,22 @@ List Mymodel_PXEM_NULL_individualcpp(
       arma::mat SigmaA_inv = construct_block(SigmaA_inv_1, SigmaA_inv_2, SigmaA_inv_2.t(), SigmaA_inv_4);
       
        if(SigmaA_inv.is_sympd()){
-         SigmaA = arma::inv_sympd(SigmaA_inv);  // ¶Ô³ÆÕı¶¨¾ØÕó×¨ÓÃ 
+         SigmaA = arma::inv_sympd(SigmaA_inv); 
          } else {
         cout << "***** SigmaA PINV *********************************************" << endl;
-        SigmaA = arma::inv(SigmaA_inv, arma::inv_opts::allow_approx);  // Î±Äæ´¦Àí²¡Ì¬Çé¿ö arma::inv(SigmaA_inv, arma::inv_opts::allow_approx)
+        SigmaA = arma::inv(SigmaA_inv, arma::inv_opts::allow_approx);  // inv(SigmaA_inv, arma::inv_opts::allow_approx)
         } 
         
-      arma::mat SigmaA_1 = SigmaA.submat(0,  0, g-1, g-1);    // ×óÉÏ¿é
-      arma::mat SigmaA_2 = SigmaA.submat(0,  g, g-1, 2*g-1);  // ÓÒÉÏ¿é
-      arma::mat SigmaA_3 = SigmaA_2.t();                      // ×óÏÂ¿é
-      arma::mat SigmaA_4 = SigmaA.submat(g,  g, 2*g-1, 2*g-1);// ÓÒÏÂ¿é 
+      arma::mat SigmaA_1 = SigmaA.submat(0,  0, g-1, g-1);    
+      arma::mat SigmaA_2 = SigmaA.submat(0,  g, g-1, 2*g-1); 
+      arma::mat SigmaA_3 = SigmaA_2.t();                      
+      arma::mat SigmaA_4 = SigmaA.submat(g,  g, 2*g-1, 2*g-1);
       
- 	  // 2. ¸üĞÂºóÑé¾ùÖµmuA(·Ö¿é¾ùÖµ) 
+ 	 
 	  muA1 = sigma2y1_inv * SigmaA_1 * M1TY1 + sigma2y2_inv * SigmaA_2 * M2TY2; 
 	  muA2 = sigma2y1_inv * SigmaA_3 * M1TY1 + sigma2y2_inv * SigmaA_4 * M2TY2;  
 	  
-	  //¼ÆËã log(¹Û²âËÆÈ»)
+	 
 	  arma::log_det(logdet1, sign1, SigmaA);
 	  arma::log_det(logdet2, sign2, Sigmaa);
 	  
@@ -184,7 +169,7 @@ List Mymodel_PXEM_NULL_individualcpp(
      }
 	  
  // M-step
-	  // 1. D1¡¢D2¡¢RµÄ¸üĞÂSigmaa
+	  // 1. D1 D2 R
 	  for(int i=0; i<g; i++){
 	  	    D1.diag()(i) = muA1(i) * muA1(i) + SigmaA_1.diag()(i);
             D2.diag()(i) = muA2(i) * muA2(i) + SigmaA_4.diag()(i);
@@ -192,14 +177,14 @@ List Mymodel_PXEM_NULL_individualcpp(
 	  } 
 	    Sigmaa = construct_block(D1, R, R.t(), D2);
 	
-	  // 3. lambdaµÄ¸üĞÂ 
+	  // 3. lambda 
 	  fenzi = arma::as_scalar(sigma2y1_inv * Y1.t() * M1 * muA1 + sigma2y2_inv * Y2.t() * M2 * muA2);
 	  fenmu1 = sigma2y1_inv * arma::as_scalar( muA1.t() * M1TM1 * muA1 ) + sigma2y2_inv * arma::as_scalar(muA2.t() * M2TM2 * muA2);
 	  fenmu2 = sigma2y1_inv * arma::trace(M1TM1 * SigmaA_1) + sigma2y2_inv * arma::trace(M2TM2 * SigmaA_4);
 	  lambda = fenzi / (fenmu1+fenmu2);
 	  lambda2 = lambda*lambda;
 	  
-	  // 2. SigmayµÄ¸üĞÂ 
+	  // 2. Sigmay
 	  Y1_temp = Y1-lambda*M1*muA1;
 	  Y2_temp = Y2-lambda*M2*muA2;
 	  
@@ -220,7 +205,7 @@ List Mymodel_PXEM_NULL_individualcpp(
 	  loglik_out= loglik.subvec(1,final_iter);
       double loglik_max = loglik(final_iter);
  
-      Rcpp::List res = Rcpp::List::create(         // ·â×°½á¹ûÁĞ±í 
+      Rcpp::List res = Rcpp::List::create(        
     _["muA1"] = muA1,
     _["muA2"] = muA2,
     _["loglik"] = loglik_out,
@@ -243,8 +228,7 @@ List Mymodel_PXEM_SA1_individualcpp(
 	const int n2,
     const int maxIter,  
     const double tol){     
- // const std::string mode,        // Ä£Ê½£¨null/alternative£©
- // const bool verbose             // µ÷ÊÔÊä³ö 
+
 
     int gNULL = M2.n_cols; 
     int N = n1+n2;
@@ -265,18 +249,17 @@ List Mymodel_PXEM_SA1_individualcpp(
 	}
 			
 			
-	//²ÎÊı³õÊ¼»¯ 
+	
 	arma::mat D1 = 0.01/g * arma::eye<arma::mat>(g, g);
 	arma::mat D2 = 0.01/gNULL * arma::eye<arma::mat>(gNULL, gNULL);
 	arma::mat R = arma::zeros<arma::mat>(g, gNULL);
-	double lambda = 1.0;    // lambdaÀ©Õ¹²ÎÊı³õÊ¼Öµ-¹Ì¶¨
-	double lambda2 = 1.0;    // lambdaÀ©Õ¹²ÎÊı³õÊ¼Öµ-¹Ì¶¨
-	double sigma2y1 = 1.0;  // sd1·½²î
-	double sigma2y2 = 1.0;  // sd2·½²î
+	double lambda = 1.0;    
+	double lambda2 = 1.0;    
+	double sigma2y1 = 1.0;  // 
+	double sigma2y2 = 1.0;  // 
 	double sigma2y1_inv = 1.0;
 	double sigma2y2_inv = 1.0;
-	 
-    //¶¨ÒåÑ­»·ÖĞÊ¹ÓÃµÄÖĞ¼ä±äÁ¿ 
+
     arma::mat M2TM2 = M2.t() * M2; 
     arma::mat M2TY2 = M2.t() * Y2; 
 
@@ -284,21 +267,20 @@ List Mymodel_PXEM_SA1_individualcpp(
 	arma::vec mediamat1;
 	arma::vec mediamat2;
 	double Tr1, Tr2, l1, l2, l3, l4, fenmu2, fenmu1, fenzi;
-	arma::vec zeros_col = arma::zeros<arma::vec>(g); // È« 0 ÁĞ
+	arma::vec zeros_col = arma::zeros<arma::vec>(g); // 0 
 
-	//¾ØÕóÆ´½ÓÓë²ğ·Ö 
+
  
-	arma::mat Sigmaa = construct_block(D1, R, R.t(), D2); //Æ´½Ó ¦²a 
+	arma::mat Sigmaa = construct_block(D1, R, R.t(), D2); // Î£a 
 
 //	cout << Sigmay_inv.n_cols <<Sigmay_inv.n_rows << endl;
 //	cout << Sigmaa.n_cols << Sigmaa.n_rows<< endl;
 	
-	//ËÆÈ»Ïà¹Ø±äÁ¿ 
-	int iter = 0;
+	
 	int final_iter;	
-	arma::vec loglik = arma::zeros<arma::vec>(maxIter);               // ´æ´¢µü´úËÆÈ»Öµ 
+	arma::vec loglik = arma::zeros<arma::vec>(maxIter);               
     double logdet1 = 0;
-	double sign1 = 0;                       // ĞĞÁĞÊ½¼ÆËã¸¨Öú±äÁ¿ 
+	double sign1 = 0;                       
 	double logdet2 = 0; 
 	double sign2 = 0;
 
@@ -307,7 +289,7 @@ List Mymodel_PXEM_SA1_individualcpp(
     arma::mat M1TY1 = M1.t() * Y1; 
 	
 	cout << "Begin: PX-EM ";
-	//Ö÷Ñ­»·
+	
 //	while(iter < 2 || (iter < maxIter && std::abs(loglik(iter - 1) - loglik(iter - 2)) >= tol)){
 
 	for (int iter = 1; iter < maxIter; iter ++ ){
@@ -315,18 +297,18 @@ List Mymodel_PXEM_SA1_individualcpp(
 	  arma::mat R_media3 = arma::zeros<arma::mat>(g, g);
 		
       // E-step 
-      // 1. ¸üĞÂºóÑéĞ­·½²î¾ØÕóSigmaA(·Ö¿éĞ­·½²îÕó) 
+      // 1. SigmaA
         if(Sigmaa.is_sympd()){
-        Sigmaa_inv = arma::inv_sympd(Sigmaa);  // ¶Ô³ÆÕı¶¨¾ØÕó×¨ÓÃ 
+        Sigmaa_inv = arma::inv_sympd(Sigmaa);  // 
          } else {
         cout << "***** Sigmaa PINV *********************************************" << endl;
-        Sigmaa_inv =arma::inv(Sigmaa, arma::inv_opts::allow_approx);  // Î±Äæ/½üËÆÄæ¾ØÕó::inv(SigmaA_inv, arma::inv_opts::allow_approx)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        Sigmaa_inv =arma::inv(Sigmaa, arma::inv_opts::allow_approx);  // ::inv(SigmaA_inv, arma::inv_opts::allow_approx)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         } 
     
-      arma::mat Sigmaa_inv_1 = Sigmaa_inv.submat(0,  0, g-1, g-1);    // ×óÉÏ¿é
-      arma::mat Sigmaa_inv_2 = Sigmaa_inv.submat(0,  g, g-1, g+gNULL-1);  // ÓÒÉÏ¿é
-      arma::mat Sigmaa_inv_3 = Sigmaa_inv_2.t();                      // ×óÏÂ¿é
-      arma::mat Sigmaa_inv_4 = Sigmaa_inv.submat(g,  g, g+gNULL-1, g+gNULL-1);// ÓÒÏÂ¿é 
+      arma::mat Sigmaa_inv_1 = Sigmaa_inv.submat(0,  0, g-1, g-1);    //
+      arma::mat Sigmaa_inv_2 = Sigmaa_inv.submat(0,  g, g-1, g+gNULL-1);  // 
+      arma::mat Sigmaa_inv_3 = Sigmaa_inv_2.t();                      // 
+      arma::mat Sigmaa_inv_4 = Sigmaa_inv.submat(g,  g, g+gNULL-1, g+gNULL-1);//  
       
       arma::mat SigmaA_inv_1 = sigma2y1_inv * M1TM1 + Sigmaa_inv_1;
       arma::mat SigmaA_inv_2 = Sigmaa_inv_2;
@@ -336,23 +318,23 @@ List Mymodel_PXEM_SA1_individualcpp(
       arma::mat SigmaA_inv = construct_block(SigmaA_inv_1, SigmaA_inv_2, SigmaA_inv_3, SigmaA_inv_4);
  
 		if(SigmaA_inv.is_sympd()){
-          SigmaA = arma::inv_sympd(SigmaA_inv);  // ¶Ô³ÆÕı¶¨¾ØÕó×¨ÓÃ 
+          SigmaA = arma::inv_sympd(SigmaA_inv);  // 
          } else {
         cout << "***** SigmaA_inV PINV *********************************************" << endl;
-        SigmaA = arma::inv(SigmaA_inv, arma::inv_opts::allow_approx);  // Î±Äæ/½üËÆÄæ¾ØÕó::inv(SigmaA_inv, arma::inv_opts::allow_approx)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        SigmaA = arma::inv(SigmaA_inv, arma::inv_opts::allow_approx);  // inv(SigmaA_inv, arma::inv_opts::allow_approx)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         } 
 	  
-      arma::mat SigmaA_1 = SigmaA.submat(0,  0, g-1, g-1);    // ×óÉÏ¿é
-      arma::mat SigmaA_2 = SigmaA.submat(0,  g, g-1, g+gNULL-1);  // ÓÒÉÏ¿é
-      arma::mat SigmaA_3 = SigmaA_2.t();                      // ×óÏÂ¿é
-      arma::mat SigmaA_4 = SigmaA.submat(g,  g, g+gNULL-1, g+gNULL-1);// ÓÒÏÂ¿é 
+      arma::mat SigmaA_1 = SigmaA.submat(0,  0, g-1, g-1);    //
+      arma::mat SigmaA_2 = SigmaA.submat(0,  g, g-1, g+gNULL-1);  // 
+      arma::mat SigmaA_3 = SigmaA_2.t();                      // 
+      arma::mat SigmaA_4 = SigmaA.submat(g,  g, g+gNULL-1, g+gNULL-1);// 
       
- 	  // 2. ¸üĞÂºóÑé¾ùÖµmuA(·Ö¿é¾ùÖµ) 
+ 	  // 2. muA
 	  arma::vec muA1 = sigma2y1_inv * SigmaA_1 * M1TY1 + sigma2y2_inv * SigmaA_2 * M2TY2; 
 	  arma::vec muA2 = sigma2y1_inv * SigmaA_3 * M1TY1 + sigma2y2_inv * SigmaA_4 * M2TY2; 
 	  //arma::vec muA = arma::join_cols(muA1, muA2); 
 	  
-	  //¼ÆËã log(¹Û²âËÆÈ»)
+	
 	  arma::log_det(logdet1, sign1, SigmaA);
 	  arma::log_det(logdet2, sign2, Sigmaa);
 	  
@@ -379,9 +361,9 @@ List Mymodel_PXEM_SA1_individualcpp(
       break;
      }
 	  // M-step
-	  // 1. R¡¢D1¡¢D2µÄ¸üĞÂSigmaa
+	 
 	  R_media = muA1*muA2.t() + SigmaA_2;
-	  R_media.shed_col(f);  // É¾³ıµÚiÁĞ 
+	  R_media.shed_col(f);  // åˆ é™¤ç¬¬iåˆ— 
 	  
 //	  cout << g << R_media.n_cols<<  R_media.n_rows<<"expected = g" << endl;
 	  
@@ -407,7 +389,7 @@ List Mymodel_PXEM_SA1_individualcpp(
 	  Sigmaa = construct_block(D1, R, R.t(), D2);
 //	  cout << Sigmaa.n_cols << Sigmaa.n_rows<< endl;
 	
-	  // 2. SigmayµÄ¸üĞÂ 
+	  // 2. Sigmay 
 	  mediamat1 = Y1-lambda*M1*muA1;
 	  mediamat2 = Y2-lambda*M2*muA2;
 	  Tr1 = arma::trace(M1 * SigmaA_1 * M1.t());
@@ -416,7 +398,7 @@ List Mymodel_PXEM_SA1_individualcpp(
 	  sigma2y2 = (1.0/n2) * ( arma::as_scalar(mediamat2.t() * mediamat2) + lambda*lambda*Tr2);
 	  sigma2y1_inv = 1.0 / sigma2y1 ;
 	  sigma2y2_inv = 1.0 / sigma2y2 ;	  
-	  // 3. lambdaµÄ¸üĞÂ 
+	  // 3. lambda 
 	  fenzi = arma::as_scalar(sigma2y1_inv * Y1.t() * M1 * muA1 + sigma2y2_inv * Y2.t() * M2 * muA2);
 	  fenmu1 = sigma2y1_inv * arma::as_scalar( muA1.t() * M1TM1 * muA1 ) + sigma2y2_inv * arma::as_scalar(muA2.t() * M2TM2 * muA2);
 	  fenmu2 = sigma2y1_inv * arma::trace(M1TM1 * SigmaA_1) + sigma2y2_inv * arma::trace(M2TM2 * SigmaA_4);
@@ -438,7 +420,7 @@ List Mymodel_PXEM_SA1_individualcpp(
   //    cout << "R :"<<R.diag() << endl;
 	}
 
-    Rcpp::List res = Rcpp::List::create(         // ·â×°½á¹ûÁĞ±í 
+    Rcpp::List res = Rcpp::List::create(        
     _["loglike_max"] = loglike_max
   );
   return res;
@@ -452,8 +434,8 @@ List Mymodel_individualcppSpecific(
 	const arma::vec y2, 
 	const arma::mat m1, 
 	const arma::mat m2,
-	const int maxIter,             // ×î´óµü´ú 
-    const double tol){     // Ğ§Ó¦·½ÏòÔ¼Êø£¨"MA"/"SA"£©
+	const int maxIter,             // 
+    const double tol){     //
 	
     Rcpp::List nullRes, SA1Res, SA2Res,output;
     arma::vec SA1_loglkmaxs;
@@ -472,7 +454,7 @@ List Mymodel_individualcppSpecific(
 	cout << "full model done.";	 
 	
 	
-	//·ÖÖÖ×å¼ìÑé 
+	 
 	SA1Res = Mymodel_PXEM_SA1_individualcpp(y1, y2, m1, m2, N1, N2,  maxIter,tol);
 	SA1_loglkmaxs = Rcpp::as<arma::vec>(SA1Res["loglike_max"]); 
 	cout << "A1 test done.";	 
@@ -485,9 +467,9 @@ List Mymodel_individualcppSpecific(
   output = Rcpp::List::create(
     _["nullRes"] = nullRes, 
     _["nullloglik_max"] = nullloglik_max, 
-    _["SA1_loglkmaxs"] = SA1_loglkmaxs,      // ¼ìÑéÍ³¼ÆÁ¿ 
-    _["SA2_loglkmaxs"] = SA2_loglkmaxs  // PÖµ 
-    // ...ÆäËûÊä³ö²ÎÊı 
+    _["SA1_loglkmaxs"] = SA1_loglkmaxs,      // 
+    _["SA2_loglkmaxs"] = SA2_loglkmaxs  // P
+    / 
   );
 
   cout << "***** done *****" << endl;
